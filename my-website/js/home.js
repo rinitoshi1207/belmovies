92a8a4531d8cd9e4b4c67a1fb3f3ed794645e6af
rinitoshi1207 <yorozukise@gmail.com>
2025-05-30 12:48:37 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = 'f2773aa21dadc4ae4c7376dd2d3da0de';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
